ecfbe5d6f2f629bb47a020eba029130c067d3820
Anna N <info@annadevelops.com>
2022-03-27 16:32:49 +0100

    Add basic pages and set up routes

---
+++ src/components/layout/Footer.jsx
@@ -0,0 +1,10 @@
+import {FaGithub} from 'react-icons/fa'
+
+function Footer() {
+  return (
+    <footer className="footer p-10 bg-gray-700 text-primary-content footer-center">
+        <FaGithub className="text-3xl"/>
+    </footer>
+  )
+}
+export default Footer

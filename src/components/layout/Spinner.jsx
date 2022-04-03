72c592dda48e35b20ae9f106081d4a7a9055524c
Anna N <info@annadevelops.com>
2022-04-03 10:22:44 +0100

    Add UserResults component and spinner icon

---
+++ src/components/layout/Spinner.jsx
@@ -0,0 +1,10 @@
+import spinner from './assets/spinner.gif'
+
+function Spinner() {
+  return (
+      <div className="w-20 mt-20 mx-auto">
+          <img src={spinner} alt="" className="text-center mx-auto" />
+      </div>
+  )
+}
+export default Spinner
